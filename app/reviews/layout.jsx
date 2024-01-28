5c58ab9818fc7f41f4307973dded44165bddccf4
pkthunder87 <pkthunder87@gmail.com>
2024-01-27 16:04:22 -0800

    Feat: - S3: [app] Rendering and Navigation - L12. Prerendering e

---
+++ app/reviews/layout.jsx
@@ -0,0 +1,8 @@
+export default function ReviewsLayout({ children }) {
+  return (
+    <div>
+      <div style={{ border: "solid red 1px" }}>[reviews menubar]</div>
+      {children}
+    </div>
+  );
+}

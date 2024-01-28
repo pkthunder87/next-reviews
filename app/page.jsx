0caa49566c800b0e604a68311968017d82cdfdaa
pkthunder87 <pkthunder87@gmail.com>
2024-01-27 18:38:15 -0800

    Feat: - L13. React Server Components

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,12 @@
+"use client";
+
+import { useEffect } from "react";
+
 export default function HomePage() {
+  useEffect(() => {
+    window.alert("Welcome to my site!!!");
+  }, []);
+
   console.log("[HomePage] rendering");
   return (
     <>

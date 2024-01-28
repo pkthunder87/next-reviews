287f4f8f7f7605a13084cc98b7027746295ddd63
pkthunder87 <pkthunder87@gmail.com>
2024-01-27 19:37:51 -0800

    Feat: - L17. Layout and Navigation

--- app/layout.jsx
+++ app/layout.jsx
@@ -23,7 +23,12 @@ export default function RootLayout({ children }) {
         </header>
 
         <main>{children}</main>
-        <footer>[footer]</footer>
+        <footer>
+          Game data and images courtesy of{" "}
+          <a href="https://rawg.io" target="_blank">
+            RAWG
+          </a>
+        </footer>
       </body>
     </html>
   );

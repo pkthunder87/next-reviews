55b3c07de99e11fa76d89edf7648f22f9c097a7b
pkthunder87 <pkthunder87@gmail.com>
2024-01-29 14:50:26 -0800

    Feat: - S4: [app] Assets and Fonts - L25. Static Assets

--- app/reviews/hollow-knight/page.jsx
+++ app/reviews/hollow-knight/page.jsx
@@ -4,6 +4,13 @@ export default function HollowKnightPage() {
   return (
     <>
       <Heading>Hollow Knight</Heading>
+      <img
+        src="/images/hollow-knight.jpg"
+        alt="Image of hollow knight video game"
+        width="640"
+        height="360"
+        className="mb-2 rounded"
+      />
       <p>This will be the review for Hollow Knight.</p>
     </>
   );
--- app/reviews/stardew-valley/page.jsx
+++ app/reviews/stardew-valley/page.jsx
@@ -4,6 +4,13 @@ export default function StardewValleyPage() {
   return (
     <>
       <Heading>Stardew Valley</Heading>
+      <img
+        src="/images/stardew-valley.jpg"
+        alt="Image of Stardew Valley video game"
+        width="640"
+        height="360"
+        className="mb-2 rounded"
+      />
       <p>This will be the review for Stardew Valley.</p>
     </>
   );

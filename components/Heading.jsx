31e51e23703c7b19141fe1c381387ae53e0b9eb6
pkthunder87 <pkthunder87@gmail.com>
2024-01-29 15:15:43 -0800

    Feat: - L27. Font Optimization

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -1,5 +1,11 @@
+import { orbitron } from '@/app/fonts';
+
 function Heading({ children }) {
-  return <h1 className="pb-3 text-2xl font-bold">{children}</h1>;
+  return (
+    <h1 className={`pb-3 text-2xl font-bold ${orbitron.className}`}>
+      {children}
+    </h1>
+  );
 }
 
 export default Heading;

a79e88d7f15a5890f7e762c873cbaab27bbf154e
pkthunder87 <pkthunder87@gmail.com>
2024-01-29 15:01:50 -0800

    Feat: - L26. Image Cards

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -6,13 +6,31 @@ export default function ReviewsPage() {
     <>
       <Heading>Reviews</Heading>
 
-      <ul>
-        <li>
-          <Link href="/reviews/hollow-knight">Hollow Knight</Link>
+      <ul className="flex flex-col gap-3">
+        <li className="w-80 rounded border bg-white shadow hover:shadow-xl">
+          <Link href="/reviews/hollow-knight">
+            <img
+              src="/images/hollow-knight.jpg"
+              alt="Image of Hollow Knight video game"
+              width="320"
+              height="180"
+              className=" rounded-t"
+            />
+            <h2 className="py-1 text-center">Hollow Knight</h2>
+          </Link>
         </li>
 
-        <li>
-          <Link href="/reviews/stardew-valley">Stardew Valley</Link>
+        <li className="w-80 rounded border bg-white shadow hover:shadow-xl">
+          <Link href="/reviews/stardew-valley">
+            <img
+              src="/images/stardew-valley.jpg"
+              alt="Image of Stardew Valley video game"
+              width="320"
+              height="180"
+              className=" rounded-t"
+            />
+            <h2 className="py-1 text-center">Stardew Valley</h2>
+          </Link>
         </li>
       </ul>
     </>

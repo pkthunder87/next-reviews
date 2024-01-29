cfeb59faf1d5c03f113385892b50bb06686e8277
pkthunder87 <pkthunder87@gmail.com>
2024-01-28 22:57:24 -0800

    Feat: - L21. Layout Styling

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,13 @@
-import Link from "next/link";
-import "./globals.css";
+import Link from 'next/link';
+import './globals.css';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="flex min-h-screen flex-col px-4 py-2">
         <header>
           <nav>
-            <ul>
+            <ul className="flex gap-2">
               <li>
                 <Link href="/">Home</Link>
               </li>
@@ -23,9 +23,9 @@ export default function RootLayout({ children }) {
           </nav>
         </header>
 
-        <main>{children}</main>
-        <footer>
-          Game data and images courtesy of{" "}
+        <main className="grow py-3">{children}</main>
+        <footer className="border-t py-3 text-center text-xs">
+          Game data and images courtesy of{' '}
           <a href="https://rawg.io" target="_blank">
             RAWG
           </a>

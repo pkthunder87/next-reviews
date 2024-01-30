2e93d7535cb7768a491653ae540e58a0f08552cd
pkthunder87 <pkthunder87@gmail.com>
2024-01-29 19:33:22 -0800

    Feat: - L30. Responsive Design

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Heading from '@/components/Heading';
 
 export default function HomePage() {
@@ -5,7 +6,24 @@ export default function HomePage() {
   return (
     <>
       <Heading>Indie Gamer</Heading>
-      <p>Only the best indie games, reviewed for you.</p>
+      <p className="pb-3">Only the best indie games, reviewed for you.</p>
+      <div className=" w-80 rounded border bg-white shadow hover:shadow-xl sm:w-full">
+        <Link
+          href="/reviews/stardew-valley"
+          className="flex flex-col sm:flex-row"
+        >
+          <img
+            src="/images/stardew-valley.jpg"
+            alt="Image of Stardew Valley video game"
+            width="320"
+            height="180"
+            className=" rounded-t sm:rounded-l sm:rounded-r-none"
+          />
+          <h2 className="py-1 text-center font-orbitron font-semibold sm:px-2">
+            Stardew Valley
+          </h2>
+        </Link>
+      </div>
     </>
   );
 }

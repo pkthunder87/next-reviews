310135a662e57762fccc7fbaf99f33676d12ce96
pkthunder87 <pkthunder87@gmail.com>
2024-01-29 21:04:14 -0800

    Feat: - L38. Rendering Arrays

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -4,42 +4,31 @@ import { getReviews } from '@/lib/reviews';
 
 export default async function ReviewsPage() {
   const reviews = await getReviews();
-  console.log('[ReviewsPage] reviews:', reviews);
 
   return (
     <>
       <Heading>Reviews</Heading>
 
-      <ul className="flex flex-col gap-3">
-        <li className="w-80 rounded border bg-white shadow hover:shadow-xl">
-          <Link href="/reviews/hollow-knight">
-            <img
-              src="/images/hollow-knight.jpg"
-              alt="Image of Hollow Knight video game"
-              width="320"
-              height="180"
-              className=" rounded-t"
-            />
-            <h2 className="py-1 text-center font-orbitron font-semibold">
-              Hollow Knight
-            </h2>
-          </Link>
-        </li>
-
-        <li className="w-80 rounded border bg-white shadow hover:shadow-xl">
-          <Link href="/reviews/stardew-valley">
-            <img
-              src="/images/stardew-valley.jpg"
-              alt="Image of Stardew Valley video game"
-              width="320"
-              height="180"
-              className=" rounded-t"
-            />
-            <h2 className="py-1 text-center font-orbitron font-semibold">
-              Stardew Valley
-            </h2>
-          </Link>
-        </li>
+      <ul className="flex flex-row flex-wrap gap-3">
+        {reviews.map((review) => (
+          <li
+            key={review.slug}
+            className="w-80 rounded border bg-white shadow hover:shadow-xl"
+          >
+            <Link href={`/reviews/${review.slug}`}>
+              <img
+                src={review.image}
+                alt=""
+                width="320"
+                height="180"
+                className=" rounded-t"
+              />
+              <h2 className="py-1 text-center font-orbitron font-semibold">
+                {review.title}
+              </h2>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );

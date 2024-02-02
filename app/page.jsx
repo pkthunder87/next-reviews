3cbd845dbbfa78ab0cc70e38368307f5cc74d2e5
pkthunder87 <pkthunder87@gmail.com>
2024-02-01 20:20:35 -0800

    Feat: - L76. Dynamic Rendering

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,8 @@ import Link from 'next/link';
 import Heading from '@/components/Heading';
 import { getReviews } from '@/lib/reviews';
 
+export const dynamic = 'force-dynamic';
+
 export default async function HomePage() {
   const featuredReviews = await getReviews(3);
 

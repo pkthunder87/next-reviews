c96358776d4b54fcde3905db95281ebc373e1e4a
pkthunder87 <pkthunder87@gmail.com>
2024-02-03 03:12:24 -0800

    Feat: - L90. Client-Only Rendering

--- components/SearchBox.jsx
+++ components/SearchBox.jsx
@@ -1,7 +1,14 @@
 'use client';
 import { Combobox } from '@headlessui/react';
+import { useIsClient } from '@/lib/hooks';
 
 function SearchBox() {
+  const isClient = useIsClient();
+
+  if (!isClient) {
+    return null;
+  }
+
   return (
     <Combobox>
       <Combobox.Input placeholder="Search…" />;
